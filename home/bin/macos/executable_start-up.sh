29fba79ff463be0553d13c9c4e2f228ba5003f89
Diego Schmidt <schmas+gh@pm.me>
2026-03-12 10:05:12 -0300

    feat(macos): add executable start-up script for launching essential applications
    This commit introduces a new script, executable_start-up.sh, that automates the opening of frequently used applications on macOS, including Zoom, Microsoft Teams, WhatsApp, Proton Mail, Brave Browser, Slack, and Notion Calendar. The script enhances user productivity by streamlining the start-up process.

---
+++ home/bin/macos/executable_start-up.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Start Up
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Start Most Used Applications
+# @raycast.author schmas
+# @raycast.authorURL https://raycast.com/schmas
+
+open "/Applications/zoom.us.app"
+sleep 0.5
+open "/Applications/Microsoft Teams.app"
+sleep 0.5
+open "/Applications/WhatsApp.app"
+sleep 0.5
+open "/Applications/Proton Mail.app"
+sleep 0.5
+open "/Applications/Brave Browser.app"
+sleep 0.5
+open "/Applications/Slack.app"
+sleep 0.5
+open "/Applications/Notion Calendar.app"
+sleep 0.5
+
